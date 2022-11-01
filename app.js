d365b0a66be5de54688b0161f0db039860f81eee
alekseyIsakin <karahunforever@gmail.com>
2022-11-01 15:03:50 +0800

    refactopring
    попытка получить json с сервера

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const express = require('express');
 const path = require('path');
 const app = express();
+const jsonParser = express.json();
 
 
 const port = 3000;
@@ -11,13 +12,29 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'static')));
 
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'static/index.html'))
 });
 
 
+app.post("/user", jsonParser, function (request, response) {
+  console.log(request.body);
+  if(!request.body) return response.sendStatus(400);
+
+  let filePath = path.join(__dirname, `static/tests/${request.body.testUUID}.json`)
+
+  let data = fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) =>{
+    if (!err)
+      response.json(JSON.parse(data));
+    else
+      console.log(err)
+  })
+});
 
 
-app.listen(port, function () {
+app.listen(port, function (error) {
+  if(!error)
+  console.log("Server is Listening at Port 3000!");
+else
+  console.log("Error Occurred");
 });
